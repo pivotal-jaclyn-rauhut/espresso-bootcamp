bf3bde908b5ddebd6543ebd6e3cb8bc4bb5e8df5
pivotal-jaclyn-rauhut <jrauhut@pivotal.io>
2016-02-03 16:49:38 -0500

    Task 3

--- app/src/androidTest/java/com/example/weatherapp/TasksTest.java
+++ app/src/androidTest/java/com/example/weatherapp/TasksTest.java
@@ -17,8 +17,12 @@ import static android.support.test.espresso.Espresso.openActionBarOverflowOrOpti
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -32,31 +36,39 @@ public class TasksTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
-//    @Test
+    //    @Test
 //    public void task0() {
 //        SystemClock.sleep(1000);
 //    }
-
+//
 //    @Test
 //    public void task1() {
-//        openActionBarOverflowOrOptionsMenu( getInstrumentation().getTargetContext());
+//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 //        onView(withText(R.string.action_settings)).perform(click());
 //        onView(withText(R.string.preference_title)).check(matches(withText("Forecast Preference")));
 //    }
 
+//    @Test
+//    public void task2() {
+//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+//        onView(withText(R.string.action_settings)).perform(click());
+//        onView(withText(R.string.preference_zip_title)).perform(click());
+//        onView(withText(R.string.preference_zip_default)).perform(replaceText("Waterloo"));
+//        onView(withText("OK")).perform(click());
+//        onView(withText("Waterloo")).check(matches(withText("Waterloo")));
+//    }
+//}
+
+//    @Test
+//    public void task3() {
+//        onView(withText("Tomorrow")).perform(swipeUp());
+//        onView(withId(R.id.snackbar_text)).check(matches(isDisplayed()));
+//    }
+
+
+
     @Test
-    public void task2() {
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        onView(withText(R.string.action_settings)).perform(click());
-        onView(withText(R.string.preference_zip_title)).perform(click());
-        onView(withText(R.string.preference_zip_default)).perform(replaceText("Waterloo"));
-        onView(withText("OK")).perform(click());
-        onView(withText(R.string.action_settings)).perform(click());
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        onView(withText(R.string.action_settings)).perform(click());
-        onView(withText(R.string.preference_zip_title)).perform(click());
-        onView(withText("Waterloo")).check(matches(withText("Waterloo")));
+    public void task4() {
+        
     }
-
-
 }
