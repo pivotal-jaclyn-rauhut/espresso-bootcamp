08620d906f86a1757b7dbe63dd9a096eeb4c5c5e
pivotal-jaclyn-rauhut <jrauhut@pivotal.io>
2016-02-10 15:33:42 -0500

    Task 6

--- app/src/androidTest/java/com/example/weatherapp/TasksTest.java
+++ app/src/androidTest/java/com/example/weatherapp/TasksTest.java
@@ -1,14 +1,14 @@
 package com.example.weatherapp;
 
 import android.os.SystemClock;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.widget.CheckedTextView;
-import android.widget.ListView;
 
 import com.example.weatherapp.activities.MainActivity;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,25 +17,14 @@ import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
-import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
-import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.object.HasToString.hasToString;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -45,42 +34,47 @@ public class TasksTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
-    //    @Test
-//    public void task0() {
-//        SystemClock.sleep(1000);
-//    }
-//
-//    @Test
-//    public void task1() {
-//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-//        onView(withText(R.string.action_settings)).perform(click());
-//        onView(withText(R.string.preference_title)).check(matches(withText("Forecast Preference")));
-//    }
+    @Ignore
+    public void task0() {
+        SystemClock.sleep(1000);
+    }
 
-//    @Test
-//    public void task2() {
-//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-//        onView(withText(R.string.action_settings)).perform(click());
-//        onView(withText(R.string.preference_zip_title)).perform(click());
-//        onView(withText(R.string.preference_zip_default)).perform(replaceText("Waterloo"));
-//        onView(withText("OK")).perform(click());
-//        onView(withText("Waterloo")).check(matches(withText("Waterloo")));
-//    }
-//}
+    @Ignore
+    public void task1() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(withText(R.string.preference_title)).check(matches(withText("Forecast Preference")));
+    }
 
-//    @Test
-//    public void task3() {
-//        onView(withText("Tomorrow")).perform(swipeUp());
-//        onView(withId(R.id.snackbar_text)).check(matches(isDisplayed()));
-//    }
-//}
+    @Ignore
+    public void task2() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(withText(R.string.preference_zip_title)).perform(click());
+        onView(withText(R.string.preference_zip_default)).perform(replaceText("Waterloo"));
+        onView(withText("OK")).perform(click());
+        onView(withText("Waterloo")).check(matches(withText("Waterloo")));
+    }
 
 
-    @Test
+    @Ignore
+    public void task3() {
+        onView(withText("Tomorrow")).perform(swipeUp());
+        onView(withId(R.id.snackbar_text)).check(matches(isDisplayed()));
+    }
+
+
+    @Ignore
     public void task4() {
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         onView(withText(R.string.action_settings)).perform(click());
         onView(withText("Temperature Units")).perform(click());
         onData(hasToString("Imperial")).perform(click());
+    }
+
+
+    @Test
+    public void task5() {
+        onView(withId(R.id.recyclerview_forecast)).perform(RecyclerViewActions.actionOnItemAtPosition(6, click()));
     }
 }
